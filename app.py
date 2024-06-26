ce28bd911f163505b4b995d3de29430bb9c572b9
Rogers Pelle <rogersdepelle@gmail.com>
2024-06-26 11:50:23 -0500

    Add Heroku deployment files

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+
+import resend
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["POST"])
+def index():
+    resend.api_key = "re_Vi5zBR4T_6gKEo9gButCJdnKgtKk8TPdp"
+    po = request.form.get("po")
+    organization = request.form.get("organization")
+    review_url = request.form.get("review_url")
+
+    po = "WSI-45HIL7"
+    organization = "Warehouse Specialists Inc"
+    review_url = "#"
+
+    params = {
+        "from": "onboarding@resend.dev",
+        "to": ["rogers@tryresponse.com"],
+        "subject": f"[PLEASE CONFIRM] PO #{po} from {organization}",
+        "html": f"""
+            <p>{organization} sent you a purchase order!</p>
+            <p>Purchase Order #{po}</p>
+            <p><b>Please review purchase order below.</b></p>
+            <br>
+            <br>
+            <p style="text-align: center">
+            <a href="{review_url}" style="background: #2F64ED; padding: 25px; color: white; font-size: 15pt; border-radius: 7px; text-decoration: none;">
+            Review Purchase Order
+            </a>
+            </p>
+            <br>
+        """,
+        "reply_to": "keivan@tryresponse.com",
+    }
+
+    return resend.Emails.send(params)
